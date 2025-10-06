d47fde3709624642b82b800a81df8e9ebe211672
槇野 <mkjljono@gmail.com>
2025-10-06 22:41:48 +0900

    更新

--- schemas/contact.py
+++ schemas/contact.py
@@ -1,13 +1,30 @@
 from pydantic import BaseModel,Field,EmailStr,HttpUrl
 from datetime import datetime
 
-
-class Contact(BaseModel):
+class ContactList(BaseModel):
     id: int
+    name: str = Field(..., min_length=2, max_length=100)
+    created_at: datetime
+    class Config:
+        from_attributes = True
+
+class ContactBase(BaseModel):
     name: str = Field(..., min_length=2, max_length=100)
     email: EmailStr
     url: HttpUrl | None = Field(default=None)
     gender: int = Field(..., strict=True, ge=0, le=2)
     message: str = Field(..., max_length=200)
     is_enabled: bool = Field(default=False)
-    created_at: datetime
+
+class ContactConfig:
+  from_attributes = True
+
+class ContactDetail(ContactBase):
+  id: int
+  created_at: datetime
+  class Config:
+    from_attributes = True
+
+
+class ContactCreate(ContactBase):
+    pass
